32976887421a88076baf57bac5344dfaf94e8979
sixuewang <sixuewang@emotibot.com>
2020-10-28 11:33:47 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+  body {
+    padding: 0;
+    margin: 0;
+    height: 100%;
+    width: 100%;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    height:100%;
+    width: 100%;
+  }
+</style>
